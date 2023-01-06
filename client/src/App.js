8d033b3042c0975a735efc1a2ce1c47ce137290b
himanshu <hmxnsu.blvck@gmail.com>
2023-01-07 01:26:45 +0530

    App.js

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  const logo = (
+    <img
+      src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png"
+      alt="logo"
+    />
+  );
+  return <div>App</div>;
+}
+
+export default App;

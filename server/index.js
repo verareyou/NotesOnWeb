a027498bf94b42901bf228166451f13c0413d3cf
himanshu <hmxnsu.blvck@gmail.com>
2023-01-16 01:08:27 +0530

    .gitignore

--- server/index.js
+++ server/index.js
@@ -1,30 +1,30 @@
 import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
 import cors from "cors";
+import {graphqlHTTP} from "express-graphql";
+import color from "colors";
+import connectDB from "./config/db.js";
+import schema from "./schema/schema.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("APP IS RUNNING");
+connectDB();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port : ${PORT}`.yellow.bold);
 });
-
-// MONGOOSE SETUP
-const CONNECTION_URL =
-  "mongodb+srv://himanshu:himanshu123@cluster0.cqh3gyf.mongodb.net/?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 6001;
-
-mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() =>
-    app.listen(PORT, () =>
-      console.log(`Server Running on Port: http://localhost:${PORT}`)
-    )
-  )
-  .catch((error) => console.log(`${error} did not connect`));
-
-// mongoose.set("useFindAndModify", false)
